08accdcf1898fc5e21f8bf3ec197fdcc5db32041
ayseturan02 <ayse0025turan@gmail.com>
2023-09-02 20:58:06 +0300

    form post structure - Creating models and controllers

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::get("/notes",[NoteController::class,"index"])->name("notes_index");
 Route::get("/notes/createPage",[NoteController::class,"createPage"])->name("notes_createPage");
 Route::post("/notes/addNote",[NoteController::class,"addNote"])->name("notes_addNote"); //store gibi
 
+
 //jetstream
 Route::middleware([
     'auth:sanctum',
